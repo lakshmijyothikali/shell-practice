c88c6abec663dae483aa2fdcae03c2012271e7d0
lakshmijyothikali <jyothidevops333@gmail.com>
2025-05-17 15:54:20 +0530

    hello world print code is done

---
+++ 01-hello-world.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#shebang it should be the first line in shell script, it is the interpreter to check the syntax and execute the commands
+
+echo "hello world"
+# echo command used to print the line
