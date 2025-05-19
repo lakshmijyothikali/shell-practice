e6b05746b5e193c182ad8f45b810982471663071
lakshmijyothikali <jyothidevops333@gmail.com>
2025-05-19 15:03:52 +0530

    varibles created in conversation file

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-echo "Trump:: Hey Musk, How are you?"
-echo "Musk:: Hello Trump, I am fine. How are you doing"
-echo "Trump: I am fine too. What's up?"
-echo "Musk:: Nohting, just going to Mars now, will you come?"
-echo "Trump:: Sorry, you carry on! I will come once you come back"
+person1=Trump
+person2=Musk
+
+echo "$person1:: Hey $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you doing"
+echo "$person1: I am fine too. What's up?"
+echo "$person2:: Nohting, just going to Mars now, will you come?"
+echo "$person1:: Sorry, you carry on! I will come once you come back"
