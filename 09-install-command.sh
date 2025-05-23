af57a494666af8c5e301aa5934909370e80de2ad
lakshmijyothikali <jyothidevops333@gmail.com>
2025-05-23 13:04:42 +0530

    09-install-command file created

--- 09-install-command.sh
+++ 09-install-command.sh
@@ -4,7 +4,17 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
- echo "ERROR : please this script run with root access"
+   echo "ERROR : please this script run with root access"
+   exit 1
  fi
 
- dnf install mysql -y
+ dnf install nginxsiud -y
+ 
+ if [ $? -eq 0 ] # $? stores exit status value other than 0 everthing consider as failure
+ then
+     echo "Installing nginx success"
+else
+    echo "Installing nginx failed"
+    exit 1
+fi
+
