7f55ae465b860cb3a971983d1fbd238259f8dda4
lakshmijyothikali <jyothidevops333@gmail.com>
2025-07-04 20:43:28 +0530

    shell practice

---
+++ script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+Greetings="Hello World"
+echo "$Greetings"
+echo "PID of the script-1:$$"
+
+./script-2
---
+++ script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "$Greetings"
+echo "PID of the script-2:$$"
